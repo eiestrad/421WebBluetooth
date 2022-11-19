f731562d22f9a8f952fa6cd046e8a3079bf5cda4
10NN <djsloan2000@gmail.com>
2022-11-18 21:14:14 -0700

    Update heartService.js

--- heartService.js
+++ heartService.js
@@ -1,22 +1,21 @@
 async function heartDevices() {
     navigator.bluetooth.requestDevice({
-        filters: [{
-            services: ['heart_rate'],
-        }]
+        optionalServices: [0x180A, 0x180F], acceptAllDevices: true
     })
-    .then(device => {
-        console.log(`Connected to '${device.name}'`);
-        return device.gatt.connect();})
-    .then(server => {
-        return server.getPrimaryService('heart_rate');
-    })
-    .then(service => {
-        return service.getCharacteristic('heart_rate_measurement');
-    })
-    .then(characteristic => {
-        return characteristic.readValue();
-    })    
-    .catch(error => { console.error(error);});
+        .then(device => {
+            console.log(`Connected to '${device.name}'`);
+            return device.gatt.connect();
+        })
+        .then(server => {
+            return server.getPrimaryService('heart_rate');
+        })
+        .then(service => {
+            return service.getCharacteristic('heart_rate_measurement');
+        })
+        .then(characteristic => {
+            return characteristic.readValue();
+        })
+        .catch(error => { console.error(error); });
 }
 
 // function characteristicValueChanged(event) {
