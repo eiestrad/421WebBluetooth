a04f8634dde0be2b87cc87577407941de23d0b70
10NN <djsloan2000@gmail.com>
2022-11-19 19:08:05 -0700

    Update heartService.js

--- heartService.js
+++ heartService.js
@@ -7,16 +7,19 @@ async function heartDevices() {
             return device.gatt.connect();
         })
         .then(server => {
-            return server.getPrimaryService(0x180E);
+            console.log(server.getPrimaryService(0x180E));
+            return server.getPrimaryService(0x180E);/*
             return server.getPrimaryService(0x180D);
-            return server.getPrimaryService('heart_rate');
+            return server.getPrimaryService('heart_rate');*/
         })
         .then(service => {
-            return service.getCharacteristic(0x2A3F);
+            console.log(service.getCharacteristic(0x2A3F));
+            return service.getCharacteristic(0x2A3F);/*
             return service.getCharacteristic(0x2A37);
-            return service.getCharacteristic('heart_rate_measurement');
+            return service.getCharacteristic('heart_rate_measurement');*/
         })
         .then(characteristic => {
+            characteristic.startNotifications();
             return characteristic.readValue();
         })
         .catch(error => { console.error(error); });
@@ -25,4 +28,5 @@ async function heartDevices() {
 function parseHeartRate(value) {
     value = value.buffer ? value : new DataView(value);
     console.log(`> Battery Level is ${value.getUint8(0)}%`);
+    heartRate.innerHTML = `${value.getUint8(0)}`;
 }
